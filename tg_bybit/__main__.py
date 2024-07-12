2cd938fb553b331171a3abb2c2ca3ea0436bffe0
ivanovsdesign <78684921+ivanovsdesign@users.noreply.github.com>
2024-07-12 15:45:08 +0300

    #1 Add Isolation Forest

--- tg_bybit/__main__.py
+++ tg_bybit/__main__.py
@@ -4,11 +4,11 @@ import asyncio
 import requests
 from aiogram import Bot, Dispatcher, Router
 from aiogram.types import (
-                    CallbackQuery,
-                    InlineKeyboardMarkup,
-                    InlineKeyboardButton,
-                    Message
-                )
+    CallbackQuery,
+    InlineKeyboardMarkup,
+    InlineKeyboardButton,
+    Message,
+)
 from aiogram.types.input_file import InputFile, BufferedInputFile
 from aiogram.types import FSInputFile
 from aiogram.enums.parse_mode import ParseMode
@@ -23,43 +23,59 @@ from utils.visualization import generate_plot
 from ta import add_all_ta_features
 from ta.volatility import BollingerBands
 
+from sklearn.ensemble import IsolationForest
+
 import pandas as pd
 
-config = dotenv_values('.env')
+config = dotenv_values(".env")
 
-bot = Bot(token = config['TG_API_TOKEN'])
+bot = Bot(token=config["TG_API_TOKEN"])
 
 dp = Dispatcher()
 router = Router()
 
-pair = 'BTCUSDT'
+pair = "BTCUSDT"
 
-async def get_bybit_data(pair, interval='1'):
+forest = IsolationForest()
+
+
+async def get_bybit_data(pair, interval="1"):
     url = f"https://api-testnet.bybit.com/v5/market/kline?category=inverse&symbol={pair}&interval={interval}&limit=15"
     response = requests.get(url)
     data = response.json()
     return data
 
+
 def calculate_statistics(data):
-    closes = [float(candle[4]) for candle in data['result']['list']]
-    opens = [float(candle[1]) for candle in data['result']['list']]
-    highs = [float(candle[2]) for candle in data['result']['list']]
-    lows = [float(candle[3]) for candle in data['result']['list']]
-    volumes = [float(candle[5]) for candle in data['result']['list']]
+    closes = [float(candle[4]) for candle in data["result"]["list"]]
+    opens = [float(candle[1]) for candle in data["result"]["list"]]
+    highs = [float(candle[2]) for candle in data["result"]["list"]]
+    lows = [float(candle[3]) for candle in data["result"]["list"]]
+    volumes = [float(candle[5]) for candle in data["result"]["list"]]
 
     return {
-        'open': opens[0],
-        'high': max(highs),
-        'low': min(lows),
-        'close': closes[-1],
-        'volume': sum(volumes)
+        "open": opens[0],
+        "high": max(highs),
+        "low": min(lows),
+        "close": closes[-1],
+        "volume": sum(volumes),
     }
+
+
+def detect_anomalies(data):
+    volumes = [float(candle[5]) for candle in data["result"]["list"]]
+    anomalies = forest.fit_predict(volumes)
+    return anomalies
+
 
 @dp.message(CommandStart())
 async def send_welcome(message: Message):
-    await message.reply('Use `stats` to get statistics on BTCUSDT pair in 15 minutes window')
+    await message.reply(
+        "Use `stats` to get statistics on BTCUSDT pair in 15 minutes window"
+    )
 
-@dp.message(Command('stats'))
+
+@dp.message(Command("stats"))
 async def send_stats(message: Message):
     data = await get_bybit_data(pair)
 
@@ -75,17 +91,17 @@ async def send_stats(message: Message):
         f"📊 Volume: {stats['volume']}"
     )
 
-    
-
-    #photo = BufferedInputFile(plot_buf, filename='temp/plot.png')
+    # photo = BufferedInputFile(plot_buf, filename='temp/plot.png')
     photo = FSInputFile("temp/plot.png")
 
     await message.reply(response_message, parse_mode=ParseMode.MARKDOWN)
     await bot.send_photo(message.chat.id, photo=photo)
 
+
 async def main() -> None:
     await dp.start_polling(bot)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     asyncio.run(main())
